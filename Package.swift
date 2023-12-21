e1daa06b971279e6bcc9ddd6f2a0c7c586a5a90b
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-12-21 15:28:25 -0600

    Updated platform

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Text",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v14),
         .iOS(.v14),
     ],
     products: [
