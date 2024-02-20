9d5420489992ccf5c3376ad2c62659b732182c2e
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-02-20 13:07:19 -0600

    Removed directory hints

--- Sources/Text/Extensions/URL+Text.swift
+++ Sources/Text/Extensions/URL+Text.swift
@@ -9,15 +9,8 @@ import Foundation
 
 public extension URL
 {
-    func appending(component: Text, isDirectory: Bool) -> URL
+    func appending(component: Text) -> URL
     {
-        if isDirectory
-        {
-            return self.appending(component: component.toUTF8String(), directoryHint: .isDirectory)
-        }
-        else
-        {
-            return self.appending(component: component.toUTF8String(), directoryHint: .notDirectory)
-        }
+        return self.appending(component: component.toUTF8String())
     }
 }
